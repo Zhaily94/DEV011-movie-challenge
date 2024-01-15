5dc7485e6dcdacae48bc1d0a59eae505dbd4818f
zhaira <zhailysweetly@gmail.com>
2024-01-15 00:17:43 -0600

    the api key was implemented and consult was done

--- movie-challenge/src/pages/Movies.js
+++ movie-challenge/src/pages/Movies.js
@@ -1,11 +1,13 @@
 // About.js
 import React from 'react';
+import MovieList from '../components/movieList.js';
 
 const Movies = () => {
   return (
     <div>
       <h2>Movies</h2>
       <p>Esta es la página "Movies".</p>
+      <MovieList/>
     </div>
   );
 };
